a652d16bf50810b51c2c0ff4cd13b872fdfe171e
onionloop <125755906+onionloop@users.noreply.github.com>
2025-09-03 20:48:19 +0530

    Initial Commit

--- alien _invasion.py
+++ alien _invasion.py
@@ -7,14 +7,16 @@ class AlienInvasion:
         pygame.init()
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption('Alien Invasion')
-
+        self.clock = pygame.time.Clock()
+        self.bg_color = (230, 230, 230)
     def run_game(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
             pygame.display.flip()
-
+            self.clock.tick(60)
+            self.screen.fill(self.bg_color)
 if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
