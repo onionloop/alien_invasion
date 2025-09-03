f16e2fcd116386c26979de907be7461e631daa52
onionloop <125755906+onionloop@users.noreply.github.com>
2025-09-03 23:49:16 +0530

    Initial Commit

--- src/ship.py
+++ src/ship.py
@@ -6,7 +6,7 @@ class Ship:
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
 
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('../images/ship.bmp')
         self.rect = self.image.get_rect()
 
         self.rect.midbottom = self.screen_rect.midbottom
