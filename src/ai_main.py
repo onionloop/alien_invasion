ba7da7b2f63702c1831ea3a2f156763e45a1ca11
onionloop <125755906+onionloop@users.noreply.github.com>
2025-09-07 17:58:19 +0530

    Initial Commit

--- src/ai_main.py
+++ src/ai_main.py
@@ -7,11 +7,14 @@ class AlienInvasion:
 
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption('Alien Invasion')
+
         self.clock = pygame.time.Clock()
         self.bg_color = (230, 230, 230)
+
         self.settings = Settings()
+
+
         self.screen = pygame.display.set_mode(
             (self.settings.screenwidth, self.settings.screenheight),
             pygame.RESIZABLE
@@ -19,12 +22,8 @@ class AlienInvasion:
 
         self.ship = Ship(self)
 
-        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
-        self.settings.screenwidth = self.screen.get_rect().width
-        self.settings.screenheight = self.screen.get_rect().height
+
         self.fullscreen = False
-
-
 
     def run_game(self):
         while True:
@@ -32,7 +31,6 @@ class AlienInvasion:
             self._update_screen()
             self.clock.tick(60)
             self.ship.update()
-
 
     def _check_events(self):
         for event in pygame.event.get():
@@ -42,31 +40,35 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
-            elif event.type == pygame.VIDEORESIZE:
+            elif event.type == pygame.VIDEORESIZE and not self.fullscreen:
                 self.settings.screenwidth, self.settings.screenheight = event.size
-
-                self.screen = pygame.display.set_mode(
-                    event.size, pygame.RESIZABLE
-                )
-
+                self.screen = pygame.display.set_mode(event.size, pygame.RESIZABLE)
                 self.ship.rect.midbottom = self.screen.get_rect().midbottom
                 self.ship.x = float(self.ship.rect.x)
 
     def _check_keydown_events(self, event):
-        if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = True
-        elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = True
-        elif event.key == pygame.K_q:
-            sys.exit()
-        elif event.key == pygame.K_F11:
-           if not self.fullscreen:
-                self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-                self.fullscreen = True
-           else:
-                self.screen = pygame.display.set_mode((self.settings.screenwidth, self.settings.screenheight),
-                    pygame.RESIZABLE)
-                self.fullscreen = False
+            if event.key == pygame.K_RIGHT:
+                self.ship.moving_right = True
+            elif event.key == pygame.K_LEFT:
+                self.ship.moving_left = True
+            elif event.key == pygame.K_q:
+                sys.exit()
+
+            elif event.key == pygame.K_f:
+                if not self.fullscreen:
+                    self.windowed_size = self.screen.get_size()
+                    self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+                    self.fullscreen = True
+                else:
+                    self.screen = pygame.display.set_mode(self.windowed_size, pygame.RESIZABLE)
+                    self.settings.screenwidth, self.settings.screenheight = self.windowed_size
+                    self.fullscreen = False
+
+            elif event.key == pygame.K_ESCAPE:
+                if self.fullscreen:
+                    self.screen = pygame.display.set_mode(self.windowed_size, pygame.RESIZABLE)
+                    self.settings.screenwidth, self.settings.screenheight = self.windowed_size
+                    self.fullscreen = False
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
@@ -74,12 +76,10 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
-
     def _update_screen(self):
         self.screen.fill(self.bg_color)
         self.ship.blitme()
         pygame.display.flip()
-
 
 
 if __name__ == '__main__':
